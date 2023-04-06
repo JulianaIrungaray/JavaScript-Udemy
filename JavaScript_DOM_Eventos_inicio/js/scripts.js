dce3dff1668c800690389f2d9b5298bf120d3152
Juliana Irungaray <julianairungaray3@gmail.com>
2023-04-06 02:52:31 -0300

    eventos

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -50,3 +50,65 @@ const navegacion = document.querySelector(".navegacion");
 navegacion.appendChild(nuevoEnlace);
 
 console.log(nuevoEnlace);
+
+
+//EVENTOS
+// console.log(1);
+
+// //callBack
+// window.addEventListener('load', function() {//load espera a que el JS y TODOS los archivos que dependen del HTML estén listos
+//     console.log(2);
+// })
+
+// window.onload = function() {
+//     console.log(3);
+// }
+
+// document.addEventListener('DOMContentLoaded', function() { //Solo espera a que se descargue el HTML, pero no espera CSS o img
+//     console.log(4);
+// })
+
+// console.log(5);
+
+
+// window.onscroll = function(){ //Se ejecuta cuando estamos haciendo scroll
+//     console.log('scrolling...');
+// }
+
+//Seleccionar elementos y asociarles un evento
+const btnEnviar = document.querySelector('.boton--primario');
+btnEnviar.addEventListener('click', function(e) {
+    console.log(e);
+
+    //cancela el evento, lo que significa que cualquier acción por 
+    //defecto que deba producirse como resultado de este evento, no ocurrirá.
+    e.preventDefault();
+
+    console.log("Enviando formulario");
+})
+
+//Eventos de los Inputs y Textarea
+const datos = { //OBJETO
+    nombre: '',
+    email: '',
+    mensaje: ''
+
+}
+
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+
+nombre.addEventListener('input', leerTexto);
+email.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+function leerTexto(e) {
+    // console.log(e.target.value);
+
+    datos[e.target.id] = e.target.value;
+
+    console.log(e.target);
+
+    console.log(datos);
+}
