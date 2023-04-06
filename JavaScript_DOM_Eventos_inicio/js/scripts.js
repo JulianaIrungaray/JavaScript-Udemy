da88bedbc95c20258c56003f517f99dafdd16425
Juliana Irungaray <julianairungaray3@gmail.com>
2023-04-05 22:57:19 -0300

    firt commit

---
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -0,0 +1,10 @@
+
+//Seleccionar un elemento de HTML:
+const heading = document.querySelector("#heading"); //Retorna 0 o 1 elementos
+console.log(heading);
+
+//Modificar el h1:
+heading.textContent = "Nuevo h1";
+
+//agregar una clase al HTML:
+// heading.classList.add("nueva-clase");
